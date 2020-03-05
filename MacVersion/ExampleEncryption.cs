c7bb7c660ff6615aaa11ca0136dbdc403d40b85d
Jason Cox <jcox@academic-net.winsor.edu>
2020-03-05 09:22:47 -0500

    Lots of comments
    Oh and it works!! Yay!

--- MacVersion/ExampleEncryption.cs
+++ MacVersion/ExampleEncryption.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿#define SHOW_WORK
+
+using System;
 namespace DataEncoding
 {
     public class ExampleEncryption : IEncryptionAlgorithm
@@ -9,17 +11,20 @@ namespace DataEncoding
         public int BlockSize => 2;
 
         /// <summary>
-        /// a /really large/ prime number
+        /// a /really large/ prime number (not the largest 64-bit prime, but close)
         /// </summary>
         public static ulong prime => (ulong)Math.Pow(2, 61) - 1;
 
         /// <summary>
-        /// Another large prime, though this one is less important that it is prime.  It must be
+        /// Another large prime, though this one is less important that it is prime.  
+        /// It must be shared publicly in order to encrypt.
         /// </summary>
         public static ulong initialState => (ulong)Math.Pow(2, 31) - 1;
 
         /// <summary>
-        /// seed for this algorithm
+        /// seed for this algorithm.  Ideally, this is negotiated between two partners
+        /// using a Diffie-Helman Key Exchange.
+        /// For Added security, a different key for each message sent.
         /// </summary>
         private ulong seed;
 
@@ -30,16 +35,24 @@ namespace DataEncoding
 
         /// <summary>
         /// current key
+        /// This is the last 16-bits of the current 64-bit state.
+        /// These are the two bytes that are XOR with a block of data to encrypt/decrypt it.
+        /// The security comes in the fact that it is practically (for a mathematically provable definition of "practical")
+        /// impossible to take this key and regenerate the state of this object or the seed used to get here.
         /// </summary>
         private ushort key
         {
             get
             {
+                // take just the last 16-bits
                 ulong temp = state % ushort.MaxValue;
                 return (ushort)temp;
             }
         }
 
+        /// <summary>
+        /// Reset this object.
+        /// </summary>
         public void Reset()
         {
             state = initialState;
@@ -63,24 +76,22 @@ namespace DataEncoding
         {
             if (block.Length != 2) throw new ArgumentException("Block Size must be 2 bytes", "block");
 
-            uint temp = (uint)256 * block[0] + block[1];
-            temp = (temp + key) % ushort.MaxValue;
+            byte[] keyBytes = { (byte)(key / 256), (byte)(key % 256) };
 
-            byte[] result = { (byte)(temp / 256), (byte)(temp % 256) };
+            /// ^ is not exponent!  it is the XOR operator!  WTF is that!?!? 
+            byte[] result = { (byte)(block[0] ^ keyBytes[0]), (byte)(block[1] ^ keyBytes[1]) };
 
-            UpdateState();
+// this code only runs if you have uncommented the '#define SHOW_WORK' line at the top of this file.
+// it prints a nice arithmetic statement that lets you see the XOR at work for each pair of bytes.
+#if SHOW_WORK
+            Console.WriteLine("\t{0} {1}", Convert.ToString((char)block[0]).PadLeft(8, ' '), Convert.ToString((char)block[1]).PadLeft(8, ' '));
+            Console.WriteLine("\t{0} {1}", Convert.ToString(block[0], 2).PadLeft(8, '0'), Convert.ToString(block[1], 2).PadLeft(8, '0'));
+            Console.WriteLine("^\t{0} {1}", Convert.ToString(keyBytes[0], 2).PadLeft(8, '0'), Convert.ToString(keyBytes[1], 2).PadLeft(8, '0'));
+            Console.WriteLine("__________________________________________");
+            Console.WriteLine("\t{0} {1}", Convert.ToString(result[0], 2).PadLeft(8, '0'), Convert.ToString(result[1], 2).PadLeft(8, '0'));
 
-            return result;
-        }
-
-        public byte[] DecryptBlock(byte[] block)
-        {
-            if (block.Length != 2) throw new ArgumentException("Block Size must be 2 bytes", "block");
-
-            uint temp = (uint)256 * block[0] + block[1];
-            temp = (temp + key) % ushort.MaxValue;
-
-            byte[] result = { (byte)(temp / 256), (byte)(temp % 256) };
+            Console.ReadLine();
+#endif
 
             UpdateState();
 
@@ -88,17 +99,32 @@ namespace DataEncoding
         }
 
         /// <summary>
+        /// In this particular algorithm, these two methods are identical!
+        /// </summary>
+        /// <param name="block"></param>
+        /// <returns></returns>
+        public byte[] DecryptBlock(byte[] block) => EncryptBlock(block);
+
+        /// <summary>
         /// Compute new state as (state ^ seed) % prime
+        /// This is really the pseudo random bit generator which is necessary to make a secure key.
         /// </summary>
         public void UpdateState()
         {
+            // hold onto the current state as it is used in the calculation
+            // We can't just say 'state = Math.Pow(state, seed);', because 
+            // that number will likely be so big you blow up your computer.
             ulong temp = state;
 
+            // These numbers are VERY large, so in order to make sure we 
+            // don't overflow a 64-bit integer, we need to do this exponent
+            // one multiplication at a time and % out the prime at each step.
             for(ulong i = 0; i < this.seed; i++)
             {
                 temp = (temp * state) % prime;
             }
 
+            // now store the new state.
             state = temp;
         }
     }
